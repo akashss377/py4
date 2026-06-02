ad8420858c41550f87cf5632aa8ae2a34f4b03b4
akashss377 <ssakash377@gmail.com>
2026-06-02 05:38:29 +0530

    new

--- day7cw.py
+++ day7cw.py
@@ -1,22 +1,16 @@
 grocery_list = ["milk", "bread", "eggs"]
-
 def add_item(item):
     grocery_list.append(item)
-
+add_item("butter")    
 def remove_last_item():
     if grocery_list:
         grocery_list.pop()
-
+remove_last_item()        
 display_item = lambda item: print(f"Item: {item}")
-
 def count_characters(items):
     if not items:
         return 0
     return len(items[0]) + count_characters(items[1:])
-
-add_item("butter")
-remove_last_item()
-
 print("Grocery List:")
 for item in grocery_list:
     display_item(item)
